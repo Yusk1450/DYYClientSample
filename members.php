613a4dd5f7be6c093e1f13867b95bbea0c8ebf14
Yusk <ishigo12@iamas.ac.jp>
2014-11-10 16:33:24 +0900

    登録者のURLがあれば、登録者名にリンクを貼る機能の実装

--- members.php
+++ members.php
@@ -9,17 +9,32 @@
 $res = json_decode(file_get_contents("http://49.212.141.66/DYY/list.php"), true);
 
 $members = array();
+$urls = array();
 
 foreach ($res as $data)
 {
 	$members[] = $data["nickname"];
+	$urls[] = $data["user_url"];
 }
 $members = array_unique($members);
 
-foreach ($members as $member)
+$html = "";
+foreach ($members as $key => $member)
 {
-	echo $member."<br>";
+	$url = $urls[$key];
+	if ($url)
+	{
+		$html .= "<a href=".$url." target=_blank>";
+	}
+	$html .= $member;
+	if ($url)
+	{
+		$html .= "</a>";
+	}
+	$html .= "<br>";
 }
+
+echo $html;
 
 ?>
 </body>
