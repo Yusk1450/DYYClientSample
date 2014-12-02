11fa4fe8095261c4aeaa2895d5944a192b36afb1
Yusk <ishigo12@iamas.ac.jp>
2014-12-02 20:53:39 +0900

    ソート機能の実装

--- index.php
+++ index.php
@@ -13,6 +13,12 @@
 			font-size: 16px;
 			font-weight: normal;
 		}
+		th a
+		{
+			color: #ffffff;
+			text-decoration: none;
+			border-bottom: 1px dotted;
+		}
 		td
 		{
 			min-width: 120px;
@@ -24,21 +30,32 @@
 <body>
 <table>
 <tr>
-	<th>手垢登録日</th>
-	<th>ニックネーム</th>
-	<th>名称</th>
-	<th>分類</th>
-	<th>場所</th>
-	<th>特徴</th>
-	<th>種類</th>
-	<th>パターン</th>
-	<th>色</th>
-	<th>形</th>
+	<th id="timestamp"><a href="index.php?s=timestamp">手垢登録日</a></th>
+	<th id="nickname"><a href="index.php?s=nickname">ニックネーム</a></th>
+	<th id="title"><a href="index.php?s=title">名称</a></th>
+	<th id="categories"><a href="index.php?s=categories">分類</a></th>
+	<th id="mark_positions"><a href="index.php?s=mark_positions">場所</a></th>
+	<th id="characteristics"><a href="index.php?s=characteristics">特徴</a></th>
+	<th id="types"><a href="index.php?s=types">種類</a></th>
+	<th id="patterns"><a href="index.php?s=patterns">パターン</a></th>
+	<th id="colors"><a href="index.php?s=colors">色</a></th>
+	<th id="shapes"><a href="index.php?s=shapes">形</a></th>
 	<th>登録用紙</th>
 </tr>
 <?php
 
 $res = json_decode(file_get_contents("http://49.212.141.66/DYY/list.php"), true);
+
+$s = $_GET["s"];
+
+if (!is_null($s))
+{
+	foreach ($res as $key => $val)
+	{
+		$key_id[$key] = $val[$s];
+	}
+	array_multisort($key_id, SORT_ASC, $res);
+}
 
 $html = "";
 
@@ -73,6 +90,21 @@ echo $html;
 
 ?>
 </table>
+
+<script src="jquery-1.7.min.js"></script>
+<script>
+
+var s_type = <?php if (!is_null($_GET["s"])) {echo "'".$_GET["s"]."'";} else {echo "'timestamp'";} ?>;
+
+if (s_type !== '')
+{
+	var sel = 'th#'+s_type;
+	var text = $(sel+' > a').html();
+	$(sel+' > a').remove();
+	$(sel).append(text);
+}
+
+</script>
 
 </body>
 </html>
