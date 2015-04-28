9114eb963eac1d9aeef1cdfb00b1629c3d7b3066
Yusk <ishigo12@iamas.ac.jp>
2015-04-28 17:51:14 +0900

    flickrの画像が取得できない不具合の修正

--- flickr_parser.php
+++ flickr_parser.php
@@ -9,14 +9,14 @@ if (is_null($url))
 }
 
 $url = trim($url);
-$contents = htmlspecialchars(file_get_contents($url));
+$context = stream_context_create(array('http'=>array('user_agent'=>'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)')));
+$contents = htmlspecialchars(file_get_contents($url, false, $context));
 if (!$contents)
 {
 	echo "failed";
 	return;
 }
-preg_match('/https:\/\/farm.\.staticflickr\.com\/.*_z\.jpg/', $contents, $matches, PREG_OFFSET_CAPTURE);
-
+preg_match('/https?:\/\/farm.\.staticflickr\.com\/.*_z\.jpg/', $contents, $matches, PREG_OFFSET_CAPTURE);
 echo $matches[0][0];
 
 ?>
